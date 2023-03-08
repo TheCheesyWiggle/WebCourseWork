72ace83f5d278a9b40b43dc4c23a465c332b40c8
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2023-03-08 11:17:18 +0000

    index layout

--- index.php
+++ index.php
@@ -10,14 +10,16 @@
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
     <body>
-    <?php
-    if (empty($_SESSION['username'])){
-        echo "<p>You are not using a registered session?<p>";
-        echo "<a href='registration.php'>Register now</a>";
-    }else{
-        echo "<h1>Welcome to Pairs</h1>";
-        echo "<a href='pairs.php'>Click here to play!</a>";
-    }
-    ?>
+    <div class="main">
+        <?php
+        if (empty($_SESSION['username'])){
+            echo "<p>You are not using a registered session?<p>";
+            echo "<a href='registration.php'>Register now</a>";
+        }else{
+            echo "<h1>Welcome to Pairs</h1>";
+            echo "<a href='pairs.php'>Click here to play!</a>";
+        }
+        ?>
+    </div>
     </body>
 </html>
