c533bd806dac352202b0aef83218c2981c8ccf1f
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2023-03-08 12:26:29 +0000

    style landing page

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-    <link rel="stylesheet" href="./global.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-  </head>
+
+<?php
+$page = "Home";
+include_once("header.php"); 
+?>
+
     <body>
-    <div class="main">
-        <?php
-        if (empty($_SESSION['username'])){
-            echo "<p>You are not using a registered session?<p>";
-            echo "<a href='registration.php'>Register now</a>";
-        }else{
-            echo "<h1>Welcome to Pairs</h1>";
-            echo "<a href='pairs.php'>Click here to play!</a>";
-        }
-        ?>
-    </div>
+        <div id="main">
+            <?php
+            if (empty($_SESSION['username'])){
+                echo "<p>You are not using a registered session?<p>";
+                echo "<a href='registration.php'>Register now</a>";
+            }else{
+                echo "<h1>Welcome to Pairs</h1>";
+                echo "<a href='pairs.php'>Click here to play!</a>";
+            }
+            ?>
+        </div>
     </body>
 </html>
