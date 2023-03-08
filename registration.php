f1180bce47e9698e1c65d9aef48cb23219a3b8c1
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2023-03-08 12:26:35 +0000

    STyling

--- registration.php
+++ registration.php
@@ -3,19 +3,18 @@
 // Username cannot have (: ” ! @ # % ˆ& * ( ) + = { } [ ] — ; : “ ’ < > ? /)
 // Avatar selector select and configure features to assemble an emoji Avatar/image
 <?php
-session_start()
+session_start();
+$page = "Registration";
+$cookie_name = "user";
+include("header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Registration</title>
-        <link rel="stylesheet" href="./global.css">
-        <link rel="icon" href="./favicon.ico" type="image/x-icon">
-    </head>
     <body>
+        <form action="index.php">
+            <label for="name">Username:</label><br>
+            <input type="text" id="name" name="name"><br>
+        </form>
+        <?php
+            $_SESSION["username"] = $_GET['name'];
+        ?>
     </body>
 </html>
