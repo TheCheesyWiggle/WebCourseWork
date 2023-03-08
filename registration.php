e27648d8bd7da4ba2ae21813877f9641a31ede72
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2023-03-08 11:09:26 +0000

    STARTED

--- registration.php
+++ registration.php
@@ -2,3 +2,20 @@
 // registration includes: Username + Avatar selector
 // Username cannot have (: ” ! @ # % ˆ& * ( ) + = { } [ ] — ; : “ ’ < > ? /)
 // Avatar selector select and configure features to assemble an emoji Avatar/image
+<?php
+session_start()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Registration</title>
+        <link rel="stylesheet" href="./global.css">
+        <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+    </body>
+</html>
