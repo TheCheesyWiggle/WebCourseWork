1c3e302d4091b38aad6ada7859c3f51ea10a1b5a
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2023-03-08 11:09:34 +0000

    STARTED

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-// landing page
-// display message "Welcome to pairs"
-// button with "Click here to play!" (hyper link to pairs.php)
-// if user isnt logged in diplay "You’re not using a registered session? Register now" (register now has hyper link to registration)
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="./global.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  </head>
+    <body>
+    <?php
+    if (empty($_SESSION['username'])){
+        echo "<p>You are not using a registered session?<p>";
+        echo "<a href='registration.php'>Register now</a>";
+    }else{
+        echo "<h1>Welcome to Pairs</h1>";
+        echo "<a href='pairs.php'>Click here to play!</a>";
+    }
+    ?>
+    </body>
+</html>
