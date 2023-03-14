330f6c89d20d299880253acbb5dd385a3a8e0670
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2023-03-14 17:00:00 +0000

    register yk

---
+++ register.php
@@ -0,0 +1,14 @@
+
+<?php
+$page = "Register";
+
+session_start();
+echo session_id().'<br>';
+
+include("header.php");
+?>
+    <body>
+        <div id="main">
+            
+        </div>
+    </bod
